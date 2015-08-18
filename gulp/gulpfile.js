aa6b97aaaf45b2f76051080bb50bad1eeca723d4
tpmanc <tpxtrime@mail.ru>
2015-08-18 21:11:05 +0300

    add slick lib, replace css, concat & minify js

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,23 +5,43 @@ var csso = require('gulp-csso');
 var watch = require('gulp-watch');
 var autoprefixer = require('gulp-autoprefixer');
 var imageop = require('gulp-image-optimization');
+var uglify = require('gulp-uglify');
 
 gulp.task('less', function() {
-	gulp.src(['../less/main.less'])
-		.pipe(less())
+    gulp.src(['../less/main.less'])
+        .pipe(less())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-		.pipe(csso())
-		.pipe(gulp.dest('../css/'));
+        .pipe(csso())
+        .pipe(gulp.dest('../production/'));
+});
+
+gulp.task('js', function() {
+    gulp.src([
+            '../js/plugins/jquery-2.1.4.min.js',
+            '../js/plugins/TweenMax.min.js',
+            '../js/plugins/jquery.ScrollMagic.min.js',
+            '../js/plugins/animation.gsap.min.js',
+            '../js/plugins/debug.addIndicators.min.js',
+            '../js/plugins/jquery.arcticmodal-0.3.min.js',
+            '../js/main.js',
+        ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('../production/'));
 });
 
 gulp.task('watch', function() {
 
     gulp.watch(['../less/*.less', '../less/*/*.css'], function(event) {
         gulp.run('less');
-    })
+    });
+
+    gulp.watch(['../js/*.js', '../js/*/*.js'], function(event) {
+        gulp.run('js');
+    });
 });
 
 
---
+++ js/main.js
@@ -0,0 +1,83 @@
+$(function(){
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            // triggerHook: 'onLeave'
+        }
+    });
+
+    var wipeAnimation = new TimelineMax()
+            .fromTo("#two", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone, delay: 2})  // in from left
+            .to('#slide1Text', 3, {opacity: 0, left: '100%', delay: 1}, '0')
+
+            .from('#slide2Title', 0.6, {autoAlpha: 0, left: '-300'}, '-=0.8')
+            .from('#b2i', 0.8, {scale: 0}, '-=0.8')
+            .from('#two hr', 0.8, {scale: 0}, '-=0.8')
+            .from('#b2l1t', 0.4, {autoAlpha: 0, left: '-320'}, '-=0.8')
+            .from('#b2l1d', 0.6, {autoAlpha: 0, left: '-340'}, '-=0.8')
+
+            .from('#b2l2t', 0.8, {autoAlpha: 0, left: '-360'}, '-=0.8')
+            .from('#b2l2d', 1, {autoAlpha: 0, left: '-380'}, '-=0.8')
+
+            .from('#b2l3t', 1.2, {autoAlpha: 0, left: '-400'}, '-=0.8')
+            .from('#b2l3d', 1.3, {autoAlpha: 0, left: '-420'}, '-=0.8')
+
+            .fromTo("#three", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone, delay: 2})  // in from right
+
+            .from('#slide3Title', 0.6, {autoAlpha: 0, left: '+300', delay: 1}, '-=0.8')
+            .from('#b3i', 0.8, {scale: 0, delay: 1}, '-=0.8')
+            .from('#three hr', 0.8, {scale: 0}, '-=0.8')
+            .from('#b3l1t', 0.4, {autoAlpha: 0, left: '+320'}, '-=0.8')
+            .from('#b3l1d', 0.6, {autoAlpha: 0, left: '+340'}, '-=0.8')
+
+            .from('#b3l2t', 0.8, {autoAlpha: 0, left: '+360'}, '-=0.8')
+            .from('#b3l2d', 1, {autoAlpha: 0, left: '+380'}, '-=0.8')
+
+            .from('#b3l3t', 1.2, {autoAlpha: 0, left: '+400'}, '-=0.8')
+            .from('#b3l3d', 1.3, {autoAlpha: 0, left: '+420'}, '-=0.8')
+
+            .from('#b3l4t', 1.2, {autoAlpha: 0, left: '+400'}, '-=0.8')
+            .from('#b3l4d', 1.3, {autoAlpha: 0, left: '+420'}, '-=0.8')
+
+            .from('#b3l5t', 1.2, {autoAlpha: 0, left: '+400'}, '-=0.8')
+            .from('#b3l5d', 1.2, {autoAlpha: 0, left: '+420'}, '-=0.8')
+
+            .from('#b3l6t', 1.2, {autoAlpha: 0, left: '+400'}, '-=0.8')
+            .from('#b3l6d', 1.2, {autoAlpha: 0, left: '+420'}, '-=0.8')
+
+            .fromTo("#four", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone, delay: 2}) // in from top
+
+            .from('#slide4Title', 0.6, {autoAlpha: 0, top: '-50', delay: 1}, '-=0.8')
+            .from('#ex1', 0.4, {autoAlpha: 0, top: '-55'}, '-=0.8')
+            .from('#ex2', 0.6, {autoAlpha: 0, top: '-60'}, '-=0.8')
+            .from('#ex3', 0.8, {autoAlpha: 0, top: '-65'}, '-=0.8')
+            .from('#ex4', 1, {autoAlpha: 0, top: '-70'}, '-=0.8')
+            .from('#ex5', 1.2, {autoAlpha: 0, top: '-75'}, '-=0.8')
+            .from('#ex6', 1.3, {autoAlpha: 0, top: '-80'}, '-=0.8')
+            .from('#ex7', 1.2, {autoAlpha: 0, top: '-85'}, '-=0.8')
+            .from('#ex8', 1.3, {autoAlpha: 0, top: '-90'}, '-=0.8')
+            .from('#ex9', 1.2, {autoAlpha: 0, top: '-95'}, '-=0.8')
+            .from('#ex10', 1.2, {autoAlpha: 0, top: '-100'}, '-=0.8')
+            .from('#ex11', 1.2, {autoAlpha: 0, top: '-105'}, '-=0.8')
+
+            .fromTo("#five", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone, delay: 2}); // in from bottom
+
+    new ScrollMagic.Scene({
+        triggerElement: "#container",
+        triggerHook: 'onLeave',
+        duration: "800%"
+    })
+    .setPin("#container")
+    .setTween(wipeAnimation)
+    .addIndicators()
+    .addTo(controller);
+
+    $('#gallery img').on('click', function(){
+        var img = $(this).data('img');
+        var c = $('<div class="box-modal" />');
+            c.html('<img src="' + img + '">');
+            c.prepend('<div class="box-modal_close arcticmodal-close">X</div>');
+            $.arcticmodal({
+                content: c
+        });
+    });
+});
