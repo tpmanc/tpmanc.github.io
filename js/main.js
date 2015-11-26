2f236df677e100238d9a686c851b996104b63549
tpmanc <tpxtrime@mail.ru>
2015-11-26 14:01:48 +0300

    smoth scroll, disable debug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 $(function(){
+    var $window = $(window);
+    var scrollTime = 1.2; //Scroll time
+    var scrollDistance = 170; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+        
+    $window.on("mousewheel DOMMouseScroll", function(event){
+        event.preventDefault(); 
+        var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+            
+        TweenMax.to($window, scrollTime, {
+            scrollTo : { y: finalScroll, autoKill:true },
+            ease: Power1.easeOut, //For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
+            autoKill: true,
+            overwrite: 5
+        });
+    });
+
     $('#gallery').slick({
         centerMode: true,
         slide: '.elem',
@@ -93,7 +111,6 @@ $(function(){
     })
     .setPin("#container")
     .setTween(wipeAnimation)
-    .addIndicators()
     .addTo(controller);
 
     $('#gallery .zoom').on('click', function(){
